e0dff70a76341d4fe19069f6c5665a8fc86db08f
hardik <hars.neo@gmail.com>
2016-06-20 18:27:58 -0400

    added models

---
+++ MSMVCAPP/Models/Album.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace MSMVCAPP.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+        public string Title { get; set; }
+        public Artist Artist { get; set; }
+    }
+}
